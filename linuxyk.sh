6ba36230b60a675930e272d4b424934e51b12805
avengerkeynes <53865740+avengerkeynes@users.noreply.github.com>
2019-09-19 10:12:47 +0800

    Create linuxyk.sh

---
+++ linuxyk.sh
@@ -0,0 +1,16 @@
+yum install -y sssd
+yum install -y realmd
+yum install -y oddjob
+yum install -y oddjob-mkhomedir
+yum install -y adcli
+yum install -y samba-common
+yum install -y samba-common-tools
+yum install -y krb5-workstation
+yum install -y openldap-clients
+yum isntall -y policycoreutils-python
+
+cat /etc/resolv.conf
+
+realm join --user=admin example.com
+
+realm list
